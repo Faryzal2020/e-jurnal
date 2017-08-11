8e71fedbdfc04810b5d0ac7faee2ac6c872cb766
Faryzal2020 <fary290796@gmail.com>
2017-08-11 13:11:58 +0700

    update sql database

--- dbkepegawaian.sql
+++ dbkepegawaian.sql
@@ -1,3 +1,21 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 11, 2017 at 08:11 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `dbkepegawaian`
 --
@@ -474,7 +492,7 @@ CREATE TABLE `jurnal` (
   `jenis_output` varchar(50) NOT NULL,
   `waktu_mulai` datetime NOT NULL,
   `waktu_selesai` datetime NOT NULL,
-  `Tanggal_Jurnal` date NOT NULL,
+  `tanggal_jurnal` date NOT NULL,
   `jenis_aktivitas` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -482,7 +500,7 @@ CREATE TABLE `jurnal` (
 -- Dumping data for table `jurnal`
 --
 
-INSERT INTO `jurnal` (`id_jurnal`, `id_aktivitas`, `nip`, `volume`, `jenis_output`, `waktu_mulai`, `waktu_selesai`, `Tanggal_Jurnal`, `jenis_aktivitas`) VALUES
+INSERT INTO `jurnal` (`id_jurnal`, `id_aktivitas`, `nip`, `volume`, `jenis_output`, `waktu_mulai`, `waktu_selesai`, `tanggal_jurnal`, `jenis_aktivitas`) VALUES
 (1, 5, '012f', 4, 'naskah', '2017-08-02 09:00:00', '2017-08-02 16:15:00', '2017-08-02', 'Umum'),
 (2, 100, '010r', 7, 'laporan per 1x survey', '2017-08-01 09:00:00', '2017-08-01 16:00:00', '2017-08-01', 'Umum'),
 (3, 321, '012f', 1, 'laporan', '2017-08-08 08:00:00', '2017-08-08 09:00:00', '2017-08-08', 'SKP'),
