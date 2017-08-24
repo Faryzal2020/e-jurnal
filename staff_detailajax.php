8847fb50dc19c2858962e16ca9c15309451f79f2
Rifqi Dhiyaulhaq Sami Miru <riefqayx@yahoo.com>
2017-08-24 14:39:06 +0700

    db pada jurnal dan fix kalender staff

--- staff_detailajax.php
+++ staff_detailajax.php
@@ -3,7 +3,7 @@ include("config.php");
 session_start();
 $staff_tanggal = $_GET['staff_tanggal_tanggal'];
                                                 
-$xeSQL = "SELECT  jurnal.id_jurnal, jurnal.volume, jurnal.jenis_output, jurnal.waktu_mulai, jurnal.waktu_selesai, jurnal.tanggal_jurnal, jurnal.jenis_aktivitas, aktivitas.nama_aktivitas, aktivitas.id_kategori, kategori.nama_kategori,aktivitas.durasi,jurnal.keterangan FROM jurnal,aktivitas,user,kategori WHERE jurnal.id_aktivitas=aktivitas.id_aktivitas AND aktivitas.id_kategori=kategori.id_kategori AND jurnal.nip=user.nip AND jurnal.Tanggal_Jurnal = '$staff_tanggal' AND jurnal.nip = '".$_SESSION['nip']."'";
+$xeSQL = "SELECT  jurnal.id_jurnal, jurnal.volume, jurnal.jenis_output, jurnal.waktu_mulai, jurnal.waktu_selesai, jurnal.tanggal_simpan, jurnal.jenis_aktivitas, aktivitas.nama_aktivitas, aktivitas.id_kategori, kategori.nama_kategori,aktivitas.durasi,jurnal.keterangan,jurnal.tanggal_kirim FROM jurnal,aktivitas,user,kategori WHERE jurnal.id_aktivitas=aktivitas.id_aktivitas AND aktivitas.id_kategori=kategori.id_kategori AND jurnal.nip=user.nip AND jurnal.tanggal_simpan = '$staff_tanggal' AND jurnal.nip = '".$_SESSION['nip']."'";
 $detail = mysqli_query($db, $xeSQL);
 
 echo "<table border='1' class='staff_tabledata' cellpadding='50' width= '100%'>
@@ -18,7 +18,8 @@ echo "<table border='1' class='staff_tabledata' cellpadding='50' width= '100%'>
 <th align='center' style='background-color: #2C383B; color: #ECECEC; text-align: center; height: 45px;'><b>Waktu Mulai</b></th>
 <th align='center' style='background-color: #2C383B; color: #ECECEC; text-align: center; height: 45px;'><b>Waktu Selesai</b></th>
 <th align='center' style='background-color: #2C383B; color: #ECECEC; text-align: center; height: 45px;'><b>Waktu Lama Kerja</b></th>
-<th align='center' style='background-color: #2C383B; color: #ECECEC; text-align: center; height: 45px;'><b>Tanggal Input Jurnal</b></th>
+<th align='center' style='background-color: #2C383B; color: #ECECEC; text-align: center; height: 45px;'><b>Tanggal Input Draft Jurnal</b></th>
+<th align='center' style='background-color: #2C383B; color: #ECECEC; text-align: center; height: 45px;'><b>Tanggal Kirim Draft Jurnal</b></th>
 <th align='center' style='background-color: #2C383B; color: #ECECEC; text-align: center; height: 45px;'><b>Keterangan</b></th>
 </tr>";
 
@@ -31,18 +32,28 @@ while($data = mysqli_fetch_row($detail))
     
     
     echo "<tr>";
-    echo "<td align=center>$data[0]</td>";
+    echo "<td align=center style='width:4%;'>$data[0]</td>";
     echo "<td align=center style='padding: 10px; max-width: 410px; width: 25%;'>$data[7]</td>";
-    echo "<td align=center>$data[6]</td>";
+    echo "<td align=center  style='width:7.6%%;'>$data[6]</td>";
     echo "<td align=center style='width:130px;'>$data[9]</td>";
-    echo "<td align=center style='width:130px;'>$data[10] Menit</td>";
+    echo "<td align=center style='width:7.6%%;'>$data[10] Menit</td>";
     echo "<td align=center>$data[1]</td>";
     echo "<td align=center style='width: 140px; padding-top: 5px; padding-bottom: 5px;'>$data[2]</td>";
     echo "<td align=center style='min-width: 100px;'>$data[3]</td>";
     echo "<td align=center style='min-width: 100px;'>$data[4]</td>";
-    echo "<td align=center style='width: 140px;'>$durasi_pekerjaan Menit</td>";
-    echo "<td align=center>$data[5]</td>";
-    echo "<td align=center style='width: 20%; min-width: 150px;'>$data[11]</td>";
+    echo "<td align=center style='width: 7.6%'>$durasi_pekerjaan Menit</td>";
+    echo "<td align=center  style='width:10%;'>$data[5]</td>";
+    if(strtotime($data[12]) > 0){
+        $kiriman=$data[12];
+        //echo "<td align=center>$data[12]</td>";
+    } else {
+        $kiriman="jurnal belum dikirim";
+    
+    }
+    echo "<td align=center  style='width:10%;'>$kiriman</td>";
+        
+    
+    echo "<td align=center style='width: 15%; min-width: 150px;'>$data[11]</td>";
     echo "</tr>";
 }
 echo "</table>";
