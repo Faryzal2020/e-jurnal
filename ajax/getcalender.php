0a9577ff1d03798af1a8b5f206460b035ce8096e
Rifqi Dhiyaulhaq Sami Miru <riefqayx@yahoo.com>
2017-08-31 16:13:24 +0700

    ilangin bekas pull

--- ajax/getcalender.php
+++ ajax/getcalender.php
@@ -1,9 +1,8 @@
 <?php
-    session_start();
-    //include ("../config.php");
-    include_once "../functions.php";
-    $namapeg = $_GET['namapeg'];
-    $niep = $_GET['niep'];
-    echo getCalender($niep,$namapeg);
-    
+   session_start();
+   include_once "../functions.php";
+   $niep = $_GET['niep'];
+   $namapeg = $_GET['namapeg'];
+     
+   echo getCalender($niep,$namapeg);
 ?>
