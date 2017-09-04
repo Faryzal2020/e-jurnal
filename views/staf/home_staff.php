6a02617938df153adc9d88bb0a45deb501471705
Rifqi Dhiyaulhaq Sami Miru <riefqayx@yahoo.com>
2017-09-04 11:06:50 +0700

    foto fixed

--- views/staf/home_staff.php
+++ views/staf/home_staff.php
@@ -14,7 +14,6 @@
 					<table class="userpaneltable" border="0">
 						<tr>
 							<td class="foto"><div class="userphoto">
-                                <a class="tampil-foto" onclick ="ubah_foto()" title='Klik untuk upload/ubah foto'>
 									  <?php 
 									  	  	$query = "SELECT * FROM user WHERE nip='".$_SESSION['nip']."'";
 									  		$q = mysqli_query($db,$query);
@@ -22,8 +21,9 @@
 
 									  		if(!empty($row['foto'])){
 										  ?>
-									  		<img  src=<?php echo "images/".$row['foto'];?>></a>
+                                            <a class="tampil-foto" onclick ="ubah_foto()" title='Klik untuk upload/ubah foto' style='background-image: url("<?php echo "images/".$row['foto'];?>"); background-size: cover; background-position: center;'></a>
 									  <?php }else{ ?>
+                                            <a class="tampil-foto" onclick ="ubah_foto()" title='Klik untuk upload/ubah foto' style='background-image: url("images/empty.jpg"); background-size: cover; background-position: center;'>
 									  		<img  src="images/empty.jpg"></a>
 									  <?php } ?></div></td>
 						</tr>
@@ -37,7 +37,7 @@
 							<td class="userid"> <?php echo $nip; ?> </td>
 						</tr>
 					</table> 
-					<div class="logoutbtn" title=""><a class="logout" title="klik untuk log-out account" href="logout.php">Logout <span class="glyphicon glyphicon-log-out"></span></a></div>
+					<div class="logoutbtn" title=""><a class="logout" title="klik untuk log-out account" href="logout.php" >Logout <span class="glyphicon glyphicon-log-out"></span></a></div>
 				</div>
 			</div>
 	    </div>
