69f737ffb9990f447ee78b40be8a4ebbe672342d
Faryzal2020 <fary290796@gmail.com>
2017-12-04 21:23:07 +0700

    menyamakan file draftjurnal admin(atasan) dengan staff, fix mekanisme refresh di tampilan validasi ketika setelah mengganti status validasi suatu jurnal, buat status validasi jurnal menjadi OK setiap jurnal tersebut diedit

--- ajax/editJurnalStaff.php
+++ ajax/editJurnalStaff.php
@@ -20,7 +20,7 @@
                   $acttype = "umum";
             }
             $ket = $_POST['edjsKeterangan'];
-            $EDJsql = "UPDATE jurnal SET id_aktivitas = '$idAct', nip = '$nip', volume = '$vol', jenis_output = '$voltype', waktu_mulai = '$mulai', waktu_selesai = '$selesai', jenis_aktivitas = '$acttype', keterangan = '$ket' WHERE id_jurnal = '$id'";
+            $EDJsql = "UPDATE jurnal SET id_aktivitas = '$idAct', nip = '$nip', volume = '$vol', jenis_output = '$voltype', waktu_mulai = '$mulai', waktu_selesai = '$selesai', jenis_aktivitas = '$acttype', keterangan = '$ket', validasi = '1' WHERE id_jurnal = '$id'";
             //echo "<script type='javascript'>alert($EDJsql);</script>";
             if(mysqli_query($db,$EDJsql)){
                   $string = "Editing jurnal dengan ID " . $id . " berhasil disimpan";
